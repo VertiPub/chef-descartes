b5da7efeb45c1761097be72d83706a83001f8e51
Abhinav Gupta <abhinav@altiscale.com>
2013-08-20 01:54:06 -0700

    separated postgresql user for descartes

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,16 +11,34 @@ include_recipe 'postgresql::server'
 include_recipe 'database::postgresql'
 include_recipe 'redis'
 
+descartes_secret = Chef::EncryptedDataBagItem.load_secret("#{node['descartes']['secretpath']}")
+postgresql_creds = Chef::EncryptedDataBagItem.load("secrets", "postgresql", descartes_secret)
+
+pg_superuser = postgresql_creds['super_user']
+pg_superuser_passwd = postgresql_creds['password'][pg_superuser]
+pg_appuser = postgresql_creds['app_user']
+pg_appuser_passwd = postgresql_creds['password'][pg_appuser]
+
 postgresql_connection_info = { :host     => 'localhost',
                                :port     => node['postgresql']['config']['port'],
-                               :username => node['postgresql']['user'],
-                               :password => node['postgresql']['password']['postgres'] 
+                               :username => pg_superuser,
+                               :password => pg_superuser_passwd
                              }
 
 postgresql_database 'descartes' do
   connection postgresql_connection_info
   action :create
 end
+
+postgresql_database_user pg_appuser do
+  connection postgresql_connection_info
+  password pg_appuser_passwd
+  database_name 'descartes'
+  privileges [:all]
+  action [:create, :grant]
+end
+
+descartes_db_url = "postgres://#{pg_appuser}:#{pg_appuser_passwd}@localhost/descartes"
 
 # Install bundler
 # Rest of the gems will be installed using bundle install
@@ -43,6 +61,7 @@ template '/etc/init.d/descartes' do
   variables(
    :install_root => node['descartes']['install_root'],
    :user => node['descartes']['user'],
+   :group => node['descartes']['group'],
    :thin_port => node['descartes']['thin_port']
   )
 end
@@ -59,23 +78,21 @@ deploy node['descartes']['install_root'] do
   repository 'git://github.com/obfuscurity/descartes.git'
   revision 'master'
   # env is needed for db migration
-  environment "DATABASE_URL" => "postgres://#{node['postgresql']['user']}:#{node['postgresql']['password']['postgres']}@localhost/descartes"
+  environment :DATABASE_URL => descartes_db_url
   # Override the default behavior i.e. to avoid symlinking database.yml(it is not present in our case)
   symlink_before_migrate ({})
   # Don't create any dir as we don't need any.
   create_dirs_before_symlink []
   # This layout modifier will create symlinks from shared folder to release directory
-  symlinks  "pids" => "pids", 
-            "logs" => "logs" 
-
+  # We don't want to create any.
+  symlinks ({}) 
+            
   before_migrate do
-    # Create directorioes in shared path
-    %w{vendor_bundle pids logs}.each do |dname|
-      directory "#{new_resource.shared_path}/#{dname}" do
-        owner new_resource.user
-        group new_resource.group
-        mode '0755'
-      end
+    # Create directory in shared path
+    directory "#{new_resource.shared_path}/vendor_bundle" do
+      owner new_resource.user
+      group new_resource.group
+      mode '0755'
     end
     # release_path contains the current release path
     directory "#{release_path}/vendor" do
@@ -92,6 +109,9 @@ deploy node['descartes']['install_root'] do
       owner new_resource.user
       group new_resource.group
       mode '0644'
+      variables(
+         :DATABASE_URL => descartes_db_url
+      )
     end
     # Install gems using bundle install. It will look for a Gemfile.lock in current release
     execute "bundle install --path=vendor/bundle --deployment" do
